6eb3a731f5c852818051846d724bbc6d80a03774
hainet <20400616+hainet@users.noreply.github.com>
2017-11-02 10:48:12 +0900

    bar-clientのユーザー・権限をfoo-clientと差別化

--- bar-client/src/main/resources/data.sql
+++ bar-client/src/main/resources/data.sql
@@ -1,12 +1,9 @@
 INSERT INTO bar_user (username) VALUES ('hainet');
-INSERT INTO bar_user (username) VALUES ('others');
 
-INSERT INTO grant (name) VALUES ('BAR_READ');
-INSERT INTO grant (name) VALUES ('BAR_WRITE');
+INSERT INTO grant (name) VALUES ('HOGE');
+INSERT INTO grant (name) VALUES ('FUGA');
+INSERT INTO grant (name) VALUES ('PIYO');
 
--- hainet
 INSERT INTO bar_user_grant (bar_user_id, grant_id) VALUES (1, 1);
 INSERT INTO bar_user_grant (bar_user_id, grant_id) VALUES (1, 2);
-
--- others
-INSERT INTO bar_user_grant (bar_user_id, grant_id) VALUES (2, 1);
+INSERT INTO bar_user_grant (bar_user_id, grant_id) VALUES (1, 3);
