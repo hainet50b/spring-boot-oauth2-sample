6cea827bab1cfdeee59162ad5a67a9484012e9de
hainet <20400616+hainet@users.noreply.github.com>
2017-11-08 16:10:41 +0900

    クライアント識別子をインメモリに保管するサンプル(JavaConfig / yml)を追加

---
+++ authorization-server/src/main/java/com/hainet/authorization/server/security/config/AuthorizationServerConfigWithInMemory.java
@@ -0,0 +1,49 @@
+package com.hainet.authorization.server.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+import java.util.ArrayList;
+
+//@Configuration
+public class AuthorizationServerConfigWithInMemory extends AuthorizationServerConfigurerAdapter {
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security
+                .checkTokenAccess("isAuthenticated()");
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                // foo-client
+                .inMemory()
+                .withClient("foo-client-id")
+                .secret("535ee78a-cc58-49aa-82f6-d976a79456ce")
+                .scopes(new ArrayList<String>() {{
+                    add("resource.read");
+                    add("resource.write");
+                }}.toArray(new String[0]))
+                .authorizedGrantTypes("authorization_code")
+                .autoApprove(new ArrayList<String>() {{
+                    add("resource.read");
+                    add("resource.write");
+                }}.toArray(new String[0]))
+                .and()
+                // bar-client
+                .withClient("bar-client-id")
+                .secret("3aefd491-61ae-4423-8e7b-bfd6fd4b15d4")
+                .scopes(new ArrayList<String>() {{
+                    add("resource.read");
+                    add("resource.write");
+                }}.toArray(new String[0]))
+                .authorizedGrantTypes("authorization_code")
+                .autoApprove(new ArrayList<String>() {{
+                    add("resource.read");
+                    add("resource.write");
+                }}.toArray(new String[0]));
+    }
+}
--- authorization-server/src/main/java/com/hainet/authorization/server/security/config/AuthorizationServerConfigWithJdbc.java
+++ authorization-server/src/main/java/com/hainet/authorization/server/security/config/AuthorizationServerConfigWithJdbc.java
@@ -5,14 +5,12 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 import javax.sql.DataSource;
 
 @Configuration
-@EnableAuthorizationServer
-public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+public class AuthorizationServerConfigWithJdbc extends AuthorizationServerConfigurerAdapter {
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
