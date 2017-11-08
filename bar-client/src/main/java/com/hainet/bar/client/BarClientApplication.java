9db4ab21121705ddf49515f98a79718930e8bc75
hainet <20400616+hainet@users.noreply.github.com>
2017-11-08 11:26:08 +0900

    bar-clientにログアウト機能を追加

--- bar-client/src/main/java/com/hainet/bar/client/BarClientApplication.java
+++ bar-client/src/main/java/com/hainet/bar/client/BarClientApplication.java
@@ -3,16 +3,12 @@ package com.hainet.bar.client;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 
 @SpringBootApplication
-@EnableOAuth2Sso
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class BarClientApplication {
 
     public static void main(String[] args) {
