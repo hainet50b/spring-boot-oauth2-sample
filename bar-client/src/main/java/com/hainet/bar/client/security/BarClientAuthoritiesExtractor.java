78600a68d5429788e4d91fd9641fe3dcdbacf689
hainet <20400616+hainet@users.noreply.github.com>
2017-11-10 14:19:28 +0900

    typoの修正

--- bar-client/src/main/java/com/hainet/bar/client/security/BarClientAuthoritiesExtractor.java
+++ bar-client/src/main/java/com/hainet/bar/client/security/BarClientAuthoritiesExtractor.java
@@ -29,5 +29,4 @@ public class BarClientAuthoritiesExtractor implements AuthoritiesExtractor {
                         .replaceAll("\\[", "")
                         .replaceAll("]", ""));
     }
-
 }
