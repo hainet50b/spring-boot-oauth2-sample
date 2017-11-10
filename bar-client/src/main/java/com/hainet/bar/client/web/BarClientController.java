730d270be51029a0d4f11e5807ffc34d0786746a
hainet <20400616+hainet@users.noreply.github.com>
2017-11-10 14:19:51 +0900

    不要なエンドポイントを除去

--- bar-client/src/main/java/com/hainet/bar/client/web/BarClientController.java
+++ bar-client/src/main/java/com/hainet/bar/client/web/BarClientController.java
@@ -1,10 +1,7 @@
 package com.hainet.bar.client.web;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class BarClientController {
@@ -12,19 +9,5 @@ public class BarClientController {
     @GetMapping("/")
     public String index() {
         return "/index";
-    }
-
-    @GetMapping("/read")
-    @PreAuthorize("hasAuthority('BAR_READ')")
-    public String read(final RedirectAttributes attr) {
-        attr.addFlashAttribute("bar_read", true);
-        return "redirect:/";
-    }
-
-    @PostMapping("/write")
-    @PreAuthorize("hasAuthority('BAR_WRITE')")
-    public String write(final RedirectAttributes attr) {
-        attr.addFlashAttribute("bar_write", true);
-        return "redirect:/";
     }
 }
