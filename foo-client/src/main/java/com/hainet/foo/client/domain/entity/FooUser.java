6d4f46c002c47a447960ed1ff9965001ba910b87
hainet <20400616+hainet@users.noreply.github.com>
2017-11-16 17:03:25 +0900

    entityを結合

--- foo-client/src/main/java/com/hainet/foo/client/domain/entity/FooUser.java
+++ foo-client/src/main/java/com/hainet/foo/client/domain/entity/FooUser.java
@@ -6,13 +6,19 @@ import org.seasar.doma.GeneratedValue;
 import org.seasar.doma.GenerationType;
 import org.seasar.doma.Id;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Data
-public class FooUser extends SsoUser {
+public class FooUser {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String username;
+
+    LocalDateTime loggedInAt;
+
+    LocalDateTime failedToLogInAt;
 }
